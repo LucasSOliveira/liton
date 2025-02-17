fc99cb0acf19d164551e3d594ec217c775c417e7
Lucas Oliveira <lucastxt@outlook.com>
2025-02-17 08:10:33 -0300

    feat: Koyeb ajust port

--- liton-backend/src/main.ts
+++ liton-backend/src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
     credentials: true,
   });
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
